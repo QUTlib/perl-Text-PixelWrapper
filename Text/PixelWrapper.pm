5d28115c773050201d052bf164e8c37fb2eb02c1
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2014-04-01 10:36:54 +1000

    convert comments to pod

--- Text/PixelWrapper.pm
+++ Text/PixelWrapper.pm
@@ -1,39 +1,33 @@
 package Text::PixelWrapper;
 
 ####################################################################
-#
-# Text::PixelWrapper
-#
-# Line wrapping to form simple paragraphs, using an approximation
-# for proportional-width fonts.
-#
-# The standard measure is a typical 10pt (13.33px) sans-serif font.
-#
-####################################################################
-#
-# Copyright 2013 Queensland University of Technology.
-# All Rights Reserved.
-#
-# This file is part of perl-Text-PixelWrapper.
-#
-# perl-Text-PixelWrapper is free software: you can
-# redistribute it and/or modify it under the terms of the GNU General
-# Public License as published by the Free Software Foundation, either
-# version 3 of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see
-# <http://www.gnu.org/licenses/>.
-#
+
+=pod
+
+=head1 NAME
+
+B<Text::PixelWrapper> - Line wrapping for proportional-width fonts.
+
+=head1 DESCRIPTION
+
+Line wrapping to form simple paragraphs, using an approximation
+for proportional-width fonts.
+
+The standard measure is a typical 10pt (13.33px) sans-serif font.
+
+=head1 METHODS
+
+=cut
+
 ####################################################################
 
 use strict;
 use warnings;
+
+BEGIN {
+    my $VERSION = '1.0';
+    *Text::PixelWrapper::VERSION = *VERSION;
+}
 
 my $fontsize = 10*(96/72);
 my @widths = (
@@ -72,16 +66,39 @@ sub lineheight
     }
 }
 
-#
-# Get the length of a string in pixels.
-#
-# This does not account for line-breaks.
-#
-# @param $string the string to measure
-# @param %opts
-#          tab  => number of space characters that matches one tab-stop
-# @return the length of $string, in pixels
-#
+
+####################################################################
+
+=pod
+
+=head2 $length = Text::PixelWrapper::pixlength( $string, [%opts] )
+
+Get the length of a string in pixels.
+
+This does not account for line-breaks.
+
+=head3 Parameters
+
+=over
+
+=item I<$string> the string to measure
+
+=item I<%opts>
+
+=over
+
+=item C<tab> => number of space characters that matches on tab-stop
+
+=back
+
+=item I<Returns> the length of C<$string>, in pixels.
+
+=back
+
+=cut
+
+####################################################################
+
 sub pixlength
 {
     my( $string, %opts ) = @_;
@@ -97,17 +114,41 @@ sub pixlength
     return $len;
 }
 
-#
-# Get the dimensions of a string, in pixels.
-#
-# @param $string the string to measure
-# @param %opts
-#          html => 1/0 whether to compress whitespace HTML-style
-#          preservenl => 1/0 whether to keep newlines (in html mode only)
-#          tab  => number of space characters that matches one tab-stop
-#          line => height of a line, in ems (default = 1.2)
-# @return ($width, $height)
-#
+
+####################################################################
+
+=pod
+
+=head2 ($width, $height) = Text::PixelWrapper::dimensions( $string, [%opts] )
+
+Get the dimensions of a string, in pixels.
+
+=head3 Parameters
+
+=over
+
+=item I<$string> the string to measure
+
+=item I<%opts>
+
+=over
+
+=item C<html> => 1/0 whether to compress whitespace HTML-style
+
+=item C<preservenl> => 1/0 whether to keep newlines (C<html> mode only)
+
+=item C<tab> => number of space characters that matches on tab-stop
+
+=item C<line> => height of a line, in ems (default = 1.2)
+
+=back
+
+=back
+
+=cut
+
+####################################################################
+
 sub dimensions
 {
     my( $string, %opts ) = @_;
@@ -153,23 +194,57 @@ sub dimensions
     return ($width, $height);
 }
 
-#
-# Wrap a string at certain pixel-widths.
-#
-# Some arbitrary widths:
-# * 936 = 72 'W's (widest ASCII character)
-# * 1008 = 72 per-milles (widest character)
-# * 960 = 72 em
-#
-# @param $string the string to chop
-# @param $width how wide to chop it (default = 936, which is 72 'W's)
-# @param %opts
-#          br => what to put between the wrapped lines (default = "\n")
-#          html => 1/0 whether to compress whitespace HTML-style
-#          preservenl => 1/0 whether to keep newlines (in html mode only)
-#          tab  => number of space characters that matches one tab-stop
-# @return a string which is line-wrapped
-#
+
+####################################################################
+
+=pod
+
+=head2 $wrapped = Text::PixelWrapper::wrap( $string, [$width], [%opts] )
+
+Wrap a string at certain pixel-widths.
+
+Some arbitrary widths:
+
+=over
+
+=item * 936 = 72 'W's (widest ASCII character)
+
+=item * 1008 = 72 per-milles (widest character)
+
+=item * 960 = 72 em
+
+=back
+
+=head3 Parameters
+
+=over
+
+=item I<$string> the string to chop
+
+=item I<$width> how wide to chop it (default = 936, which is 72 'W's)
+
+=item I<%opts>
+
+=over
+
+=item C<br> => what to put between the wrapped lines (default = "\n")
+
+=item C<html> => 1/0 whether to compress whitespace HTML-style
+
+=item C<preservenl> => 1/0 whether to keep newlines (C<html> mode only)
+
+=item C<tab> => number of space characters that matches on tab-stop
+
+=back
+
+=item I<Returns> a string which is line-wrapped
+
+=back
+
+=cut
+
+####################################################################
+
 sub wrap
 {
     my( $string, $width, %opts ) = @_;
@@ -249,4 +324,33 @@ sub wrap
     }
     return $wrapped . ($wrapped ? $br : '') . $accum;
 }
+
+__END__
+
+=head1 AUTHOR
+
+Matthew Kerwin <matthew@kerwin.net.au>
+
+=head1 LICENSE
+
+Copyright 2013,2014 Queensland University of Technology.
+All Rights Reserved.
+
+This file is part of perl-Text-PixelWrapper.
+
+perl-Text-PixelWrapper is free software: you can
+redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+
+=cut
 
